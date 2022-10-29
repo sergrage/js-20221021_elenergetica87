a7ed0ac0ecf55c41a34a6164ed24dc30de34acc2
Sergey Shmatovskiy <elenergetica87@mail.ru>
2022-10-29 23:31:12 +0300

    add comments. change var names

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -6,26 +6,29 @@
  */
 export function trimSymbols(string, size) {
   if (size === undefined) { return string;}
-  let tempStr = string.length > 0 ? string[0] : '';
-  let tempArrOfIndex = [];
-  let index = string.length > 0 ? 1 : 0;
-  let stringArr = [...string];
+
+  let char = string.length > 0 ? string[0] : ''; // сюда записываем символ. если есть, уже записываем первый.
+  let arrOfCharLengths = [];// сюда складываем число символов идущих подряд
+  let numberOfChar = string.length > 0 ? 1 : 0; //  число символов идущих подряд. если есть первйы символ, уже записываем 1.
   let result = [];
+
+  let stringArr = [...string];
   // в данном цикле получаем массив длинн
-  // строка 'aaabbaa'  => [3,2,2]
+  // строка "aaabbaa"  => [3,2,2]
   for (let i = 1; i < stringArr.length; i++) {
-    if (tempStr === stringArr[i]) {
-      index++;
+    if (char === stringArr[i]) {
+      numberOfChar++;
     } else {
-      tempArrOfIndex.push(index);
-      index = 1;
-      tempStr = stringArr[i];
+      arrOfCharLengths.push(numberOfChar);
+      numberOfChar = 1;
+      char = stringArr[i];
     }
   }
-  // эта строчка закидывает последний index в массив
-  tempArrOfIndex.push(index);
+  // эта строчка закидывает последний numberOfChar в массив
+  arrOfCharLengths.push(numberOfChar);
+
   // тут зная длинну для каждой буквы - режем массив исходных символов на куски
-  for (let len of tempArrOfIndex) {
+  for (let len of arrOfCharLengths) {
     let removed = stringArr.splice(0, len);
     if (removed.length > size) {
       removed = removed.splice(0, size);
