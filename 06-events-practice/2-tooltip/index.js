4d56f8fab8d8df26f125285a65a8f7c632b340d3
Sergey Shmatovskiy <elenergetica87@mail.ru>
2022-11-13 17:20:52 +0300

    oop-6 tooltip fix-finish

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,47 +1,56 @@
 class Tooltip {
-  initialize () {
-    this.initEventListeners();
+  static _instance;
+  onPointerOver = event => {
+    const element = event.target.closest('[data-tooltip]');
+    if (element) {
+      this.tooltip = element.dataset.tooltip;
+      this.render(this.tooltip);
+      element.addEventListener('pointermove', this.onPointerMove);
+    }
   }
-
+  onPointerMove = (event) => {
+    this.moveTooltip(event);
+  }
+  onPointerOut = () => {
+    this.remove();
+    document.removeEventListener('pointermove', this.onPointerMove);
+  }
   constructor() {
     if (!Tooltip._instance) {
       Tooltip._instance = this;
     }
     return Tooltip._instance;
   }
-
+  initialize () {
+    this.initEventListeners();
+  }
   getTemplate(tooltip = '') {
     return `<div class="tooltip">${tooltip}</div>`;
   }
-  render(template, x, y) {
+  moveTooltip = (event) => {
+    this.element.style.left = 10 + event.clientX + 'px';
+    this.element.style.top = 10 + event.clientY + 'px';
+  }
+  render(template) {
     const element = document.createElement("div"); // (*)
     element.innerHTML = this.getTemplate(template);
     this.element = element.firstElementChild;
-    this.element.style.left = x;
-    this.element.style.top = y;
+
     document.body.append(this.element);
   }
   initEventListeners() {
-    document.addEventListener('pointerover', this.pointerover);
+    document.addEventListener('pointerover', this.onPointerOver);
+    document.addEventListener('pointerout', this.onPointerOut);
   }
-  pointerover = event => {
-    const el = event.target.closest('[data-tooltip]');
-    if (el) {
-      this.tooltip = el.dataset.tooltip;
-      this.render(this.tooltip, event.clientX + 'px', event.clientY + 'px');
-      el.addEventListener('pointerout', this.pointerout);
-    }
-  }
-  pointerout = () => {
-    this.remove();
-  }
+
   remove() {
     this.element.remove();
   }
   destroy() {
     this.remove();
-    document.removeEventListener('pointerover', this.pointerover);
-    document.removeEventListener('pointerout', this.pointerout);
+    document.removeEventListener('pointerover', this.onPointerOver);
+    document.removeEventListener('pointerout', this.onPointerOut);
+    document.removeEventListener('pointermove', this.onPointerMove);
     // NOTE: удаляем обработчики событий, если они есть
   }
 }
